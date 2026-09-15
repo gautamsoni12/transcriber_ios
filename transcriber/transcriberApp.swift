607eb0184eb94c2886672300e82ceef17c737778
Gautam Soni <gtmsoni@icloud.com>
2026-09-14 18:25:13 -0700

    Initial Commit

---
+++ transcriber/transcriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  transcriberApp.swift
+//  transcriber
+//
+//  Created by Gotham Soni on 2026-09-14.
+//
+
+import SwiftUI
+
+@main
+struct transcriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
